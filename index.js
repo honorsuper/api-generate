10c237f4e13fc590acfa47c11a0aff06c0174c99
honorsuper <honorsuper@126.com>
2022-05-26 22:21:35 +0800

    feat: api文档生成

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
 const vscode = require("vscode");
+const docgen = require("react-docgen-typescript");
 const fs = require("fs");
+const prettier = require("prettier");
+
+const options = {
+  savePropValueAsString: true,
+};
+
+function commentToMarkDown(componentInfo) {
+  let { props } = componentInfo;
+  const markdownInfo = renderMarkDown(props);
+  // 使用prettier美化格式
+  const content = prettier.format(markdownInfo, {
+    parser: "markdown",
+  });
+  return content;
+}
+
+function renderMarkDown(props) {
+  return `
+  # 组件
+  
+  ## 参数 Props
+
+  | 参数 |  说明 | 类型 | 默认值 | 必填 |
+  | --- | --- | --- | --- | ---|
+  ${Object.keys(props)
+    .map((key) => renderProp(key, props[key]))
+    .join("")}
+  `;
+}
+
+function getType(type) {
+  const handler = {
+    enum: (type) =>
+      type.value.map((item) => item.value.replace(/'/g, "")).join(" \\| "),
+    union: (type) => type.value.map((item) => item.name).join(" \\| "),
+  };
+  if (typeof handler[type.name] === "function") {
+    return handler[type.name](type).replace(/\|/g, `&nbsp;&#124;&nbsp;`);
+  } else {
+    return type.name.replace(/\|/g, `&nbsp;&#124;&nbsp;`);
+  }
+}
+
+// 渲染1行属性
+function renderProp(
+  name,
+  { type = { name: "-" }, defaultValue = { value: "-" }, required, description }
+) {
+  return `| ${name} | ${description || "-"}|${getType(type)} | ${
+    defaultValue?.value?.replace(/\|/g, "<span>|</span>") || "-"
+  } | ${required ? "true" : "false"} | 
+  `;
+}
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -12,6 +66,7 @@ function activate(context) {
   let commandOfGetFileState = vscode.commands.registerCommand(
     "start",
     (uri) => {
+      console.log(",,,", uri);
       // 文件路径
       const filePath = uri.path.substring(1);
       fs.stat(filePath, (err, stats) => {
@@ -26,24 +81,11 @@ function activate(context) {
         }
 
         if (stats.isFile()) {
-          const size = stats.size;
-          const createTime = stats.birthtime.toLocaleString();
-          const modifyTime = stats.mtime.toLocaleString();
-
-          vscode.window.showInformationMessage(
-            `
-					文件大小为:${size}字节;
-					文件创建时间为:${createTime};
-					文件修改时间为:${modifyTime}
-				`,
-            { modal: true }
-          );
+          const res = docgen.parse(filePath, options);
+          vscode.env.clipboard.writeText(commentToMarkDown(res[0]));
+          vscode.window.showInformationMessage("API文档已复制到剪切板");
         }
       });
-
-      const stats = fs.statSync(filePath);
-      console.log("stats", stats);
-      console.log("isFile", stats.isFile());
     }
   );
 
